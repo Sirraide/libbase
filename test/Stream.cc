0ae17df2d288bb4b82b5c086ff7afdb2f220d207
Sirraide <aeternalmail@gmail.com>
2024-08-22 03:56:40 +0200

    [Stream] Add remove() and remove_all()

--- test/Stream.cc
+++ test/Stream.cc
@@ -1,3 +1,14 @@
 #include "TestCommon.hh"
 
 import base;
+using namespace base;
+
+TEST_CASE("stream::remove()") {
+    std::string s = "hello world";
+    CHECK(stream{s}.remove('l') == "heo word");
+    CHECK(stream{s}.remove_all("l") == "heo word");
+    CHECK(stream{s}.remove_all("lo") == "he wrd");
+    CHECK(stream{s}.remove_all("ol") == "he wrd");
+    CHECK(stream{s}.remove('x') == "hello world");
+    CHECK(stream{s}.remove_all("xyz") == "hello world");
+}
