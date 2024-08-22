2e946ccdb44c66f7d812087cd53ae34e83072fed
Sirraide <aeternalmail@gmail.com>
2024-08-22 04:19:20 +0200

    [Stream] Add stream::split()

--- test/Stream.cc
+++ test/Stream.cc
@@ -3,6 +3,8 @@
 import base;
 using namespace base;
 
+using Catch::Matchers::RangeEquals;
+
 struct StringWrapper {
     std::string data;
     void operator +=(std::string_view s) { data += s; }
@@ -25,4 +27,26 @@ TEST_CASE("stream::remove()") {
     CHECK(stream{s}.remove_all<StringWrapper>("ol") == "he wrd");
     CHECK(stream{s}.remove<StringWrapper>('x') == "hello world");
     CHECK(stream{s}.remove_all<StringWrapper>("xyz") == "hello world");
+}
+
+TEST_CASE("stream::split()") {
+    std::string s = "aa\nbb\ncc\nbb\ncc";
+
+    SECTION ("1") {
+        auto lines = stream{s}.split("\n");
+        REQUIRE(rgs::distance(lines) == 5);
+        CHECK_THAT(lines, RangeEquals(std::vector{"aa"sv, "bb"sv, "cc"sv, "bb"sv, "cc"sv}));
+    }
+
+    SECTION ("2") {
+        auto bb = stream{s}.split("bb");
+        REQUIRE(rgs::distance(bb) == 3);
+        CHECK_THAT(bb, RangeEquals(std::vector{"aa\n"sv, "\ncc\n"sv, "\ncc"sv}));
+    }
+
+    SECTION ("3") {
+        auto cc = stream{s}.split("cc");
+        REQUIRE(rgs::distance(cc) == 3);
+        CHECK_THAT(cc, RangeEquals(std::vector{"aa\nbb\n"sv, "\nbb\n"sv, ""sv}));
+    }
 }
