8370a6b37cf8bcbe1c8a1cc5b33601961e315c4e
Sirraide <aeternalmail@gmail.com>
2024-12-25 19:08:46 +0100

    [Formatters] Use join()

--- include/base/Formatters.hh
+++ include/base/Formatters.hh
@@ -3,20 +3,13 @@
 
 #include <format>
 #include <vector>
+#include <base/Utils.hh>
 
 template <typename Inner>
 struct std::formatter<std::vector<Inner>> : std::formatter<Inner> {
     template <typename FormatContext>
     auto format(const std::vector<Inner>& vec, FormatContext& ctx) const {
-        ctx.out() = "["sv;
-        bool first = true;
-        for (const auto& elem : vec) {
-            if (first) first = false;
-            else ctx.out() = ", "sv;
-            ctx.out() = std::format("{}", elem);
-        }
-        ctx.out() = "]"sv;
-        return ctx.out();
+        return std::format_to(ctx.out(), "[{}]", base::utils::join(vec));
     }
 };
 
