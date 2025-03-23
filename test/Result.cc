b2e020ce6545fddab29d08ae94a13dc154d79e1c
Sirraide <aeternalmail@gmail.com>
2025-03-23 22:56:18 +0100

    [Tests] Add result test

--- test/Result.cc
+++ test/Result.cc
@@ -8,6 +8,14 @@ auto get_int() -> Result<int&> {
     return x;
 }
 
+struct MoveOnlyType {
+    LIBBASE_MOVE_ONLY(MoveOnlyType);
+
+public:
+    int x;
+    MoveOnlyType(int x) : x(x) {}
+};
+
 TEST_CASE("Result<T&> is unwrapped properly") {
     auto DoIt = [] -> Result<> {
         CHECK(Try(get_int()) == 42);
@@ -17,4 +25,18 @@ TEST_CASE("Result<T&> is unwrapped properly") {
     };
 
     DoIt().value();
+}
+
+TEST_CASE("Result<MoveOnlyType>") {
+    auto CopyInit = [] -> Result<MoveOnlyType> {
+        return MoveOnlyType{42};
+    };
+
+    auto NRVO = [] -> Result<MoveOnlyType> {
+        MoveOnlyType x{42};
+        return x;
+    };
+
+    CHECK(CopyInit().value().x == 42);
+    CHECK(NRVO().value().x == 42);
 }
