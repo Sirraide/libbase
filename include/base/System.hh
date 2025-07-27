4dd82035a549018ae8f242accde14abee9fb9802
Sirraide <aeternalmail@gmail.com>
2025-07-27 10:06:20 +0200

    [System] Add comment

--- include/base/System.hh
+++ include/base/System.hh
@@ -9,6 +9,12 @@
 
 namespace base::sys {
 #ifdef __unix__
+/// Log to a file descriptor in a signal-safe manner.
+///
+/// \param msg The message to print.
+/// \param fd The file to print to; defaults to stderr.
+///
+/// This is only available on UNIX systems.
 void atomic_log(std::string_view msg, int fd = STDERR_FILENO);
 #endif
 
